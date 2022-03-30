7851336efafdc4624d4007029d0be18eed2dea35
johnatan-ferre <johnatanferre@gmail.com>
2022-03-30 10:59:31 -0300

    Se agregan datos al array

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const lugares = [];
-let totalLugares = 8;
 
 
 class Lugar {
@@ -11,6 +10,17 @@ class Lugar {
         this.rutaprincipal = rutaprincipal;
     }
 }
+
+lugares.push(new Lugar("Valle Lunarejo", "Rivera", 477, 6, "Ruta 30"));
+lugares.push(new Lugar("Montes del Queguay", "Paysandu", 387, 4, "Ruta 4"));
+lugares.push(new Lugar("Quebrada de los Cuervos", "Treinta y Tres", 330, 3, "Ruta 8"));
+lugares.push(new Lugar("Humedales Santa Lucía", "Montevideo", 22, 40, "Ruta 1"));
+lugares.push(new Lugar("Cerro Verde", "Rocha", 303, 4, "Ruta 9"));
+lugares.push(new Lugar("Cabo Polonio", "Rocha", 272, 10, "Ruta 10"));
+lugares.push(new Lugar("Esteros de Farrapos", "Rio Negro", 357, 8, "Ruta 24"));
+lugares.push(new Lugar("Laguna de Castillos", "Rocha", 241, 5, "Ruta 9"));
+lugares.push(new Lugar("Laguna Garzon", "Maldonado", 178, 12, "Ruta 10"));
+lugares.push(new Lugar("Grutas del Palacio", "Flores", 231, 8, "Ruta 14"));
 
 
 
